e65c92cb152546b99f874507ccdce44fb0ba1434
Rootskery <cyrilimperial12@gmail.com>
2026-02-07 01:56:54 +0800

    Initialize clean architecture backend with Dms.Api, Application, Domain, Infrastructure projects

---
+++ Dms.Application/DTOs/UserDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dms.Application.DTOs
+{
+    internal class UserDto
+    {
+    }
+}
